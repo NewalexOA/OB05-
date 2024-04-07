c487df3303861caf32d280b543149db7e72c98df
Anashkin Aleksei <109337222+NewalexOA@users.noreply.github.com>
2024-04-07 16:10:29 +0300

    add main window code

--- main.py
+++ main.py
@@ -1,36 +1,17 @@
 import pygame
-pygame.init()
+import sys
 
-window_size = (800, 600)
-screen = pygame.display.set_mode(window_size)
-pygame.display.set_caption("Test project")
+def run_game():
+    pygame.init()
+    screen = pygame.display.set_mode((600, 800))
+    pygame.display.set_caption("Tetris PY")
 
-image = pygame.image.load("img/pic.png")
-image = pygame.transform.scale(image, (50, 50))
-image_rect = image.get_rect()
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
 
-speed = 5
+        pygame.display.flip()
 
-run = True
-
-while run:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT]:
-        image_rect.x -= speed
-    if keys[pygame.K_RIGHT]:
-        image_rect.x += speed
-    if keys[pygame.K_UP]:
-        image_rect.y -= speed
-    if keys[pygame.K_DOWN]:
-        image_rect.y += speed
-
-
-    screen.fill((0, 0, 0))
-    screen.blit(image, image_rect)
-
-    pygame.display.flip()
-
-pygame.quit()
+if __name__ == '__main__':
+    run_game()
